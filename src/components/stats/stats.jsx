7c5410aff0c5a9cb433fdb318b1f9a03bd2630cc
brokenhoax <brokenhoax@outlook.com>
2020-09-02 02:48:36 -0500

    Updated Stats Page Stylings

--- src/components/stats/stats.jsx
+++ src/components/stats/stats.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import styles from "./Stats.module.css";
 import CountHours from "../charts/countHours/CountHours";
 import CountWebinars from "../charts/countWebinars/CountWebinars";
@@ -8,50 +8,48 @@ import Presenter from "../charts/presenter/Presenter";
 import Skill from "../charts/skill/Skill";
 
 class Stats extends Component {
-
-    render() { 
-        return ( 
-            <div className={styles.chartContainer}>
-                <CountWebinars 
-                    chart={styles.chart}
-                    chartHeader={styles.chartHeader} 
-                    webinars={styles.totalWeb}
-                    texty={styles.texty}
-                    />
-                <CountHours 
-                    chart={styles.chart}
-                    chartHeader={styles.chartHeader} 
-                    hours={styles.hours}
-                    />
-                <Timeline 
-                    chart={styles.chart}
-                    chartHeader={styles.chartHeader} 
-                    webinars={styles.webinars}
-                    />
-                <Genre 
-                    chart={styles.chart}
-                    chartHeader={styles.chartHeader} 
-                    webinars={styles.webinars}
-                    genre={styles.genre}
-                    />
-                <Presenter 
-                    chart={styles.chart}
-                    chartHeader={styles.chartHeader} 
-                    webinars={styles.webinars}
-                    />
-                <Skill 
-                    chart={styles.chart}
-                    chartHeader={styles.chartHeader} 
-                    webinars={styles.webinars}
-                    />
-            </div>
-         );
-    }
+  render() {
+    return (
+      <div className={styles.chartContainer}>
+        <CountWebinars
+          chart={styles.chart}
+          chartHeader={styles.chartHeader}
+          totalWeb={styles.totalWeb}
+          webinars={styles.webinars}
+        />
+        <CountHours
+          chart={styles.chart}
+          chartHeader={styles.chartHeader}
+          totalHours={styles.totalHours}
+          hours={styles.hours}
+        />
+        <Timeline
+          chart={styles.chart}
+          chartHeader={styles.chartHeader}
+          timeline={styles.timeline}
+        />
+        <Genre
+          chart={styles.chart}
+          chartHeader={styles.chartHeader}
+          genre={styles.genre}
+        />
+        <Presenter
+          chart={styles.chart}
+          chartHeader={styles.chartHeader}
+          presenter={styles.presenter}
+        />
+        <Skill
+          chart={styles.chart}
+          chartHeader={styles.chartHeader}
+          skill={styles.skill}
+        />
+      </div>
+    );
+  }
 }
 
-
 // getStatsClass() {
-//     let statsClass = 
+//     let statsClass =
 // }
- 
+
 export default Stats;
