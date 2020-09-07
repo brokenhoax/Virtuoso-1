8880abf82ec271b34e77a3e5721e59c2d0ee33e1
brokenhoax <brokenhoax@outlook.com>
2020-09-07 17:48:55 -0500

    Fixed Travis Bug. Missing File Extension on Import

--- src/test/logo.test.js
+++ src/test/logo.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import Logo from "../components/logo/Logo";
+import Logo from "../components/logo/Logo.jsx";
 
 test("renders learn react link", () => {
   const { getByText } = render(<Logo />);
