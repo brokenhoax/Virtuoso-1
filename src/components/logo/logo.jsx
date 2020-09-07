22c597b63f147fd9f6973e23e2829698c5a0b299
brokenhoax <brokenhoax@outlook.com>
2020-09-07 17:11:07 -0500

    Fixed Travis Logo Text by Adding BrowserRouter around Link and Importing

--- src/components/logo/logo.jsx
+++ src/components/logo/logo.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 import styles from "./Logo.module.css";
-import { Link } from "react-router-dom";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 
 class Logo extends Component {
   render() {
     return (
-      <Link to="/" className={styles.logo}>
-        V
-      </Link>
+      <Router>
+        <Link to="/" className={styles.logo}>
+          V
+        </Link>
+      </Router>
     );
   }
 }
--- src/test/logo.test.js
+++ src/test/logo.test.js
@@ -1,9 +1,9 @@
 // Use this as an example test????
-import React from 'react';
-import { render } from '@testing-library/react';
-import Logo from '../components/logo/logo';
+import React from "react";
+import { render } from "@testing-library/react";
+import Logo from "../components/logo/Logo";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   const { getByText } = render(<Logo />);
   const logo = getByText(/V/);
   expect(logo).toBeInTheDocument();
