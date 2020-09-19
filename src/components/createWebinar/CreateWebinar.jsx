e0b464427d8f9eead1baa0cd6617c63fd02647ef
Andrew <brokenhoax@pop-os.localdomain>
2020-09-19 14:54:54 -0500

    Fixed Photo Names To Support Image Render

--- src/components/createWebinar/CreateWebinar.jsx
+++ src/components/createWebinar/CreateWebinar.jsx
@@ -92,10 +92,6 @@ class CreateWebinar extends Component {
         }
     }
 
-
-
-    
-
     render() {
         let day = ['--', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31'];
         let dayOptions = day.length > 0 && day.map((num, i) => {
@@ -306,7 +302,6 @@ class CreateWebinar extends Component {
                 />
               </section>
             </div>
-
             <section className={styles.videoURL}>
               Video URL:
               <input
