850158f59f8411907fc650d7b649c004b3d22855
Andrew <brokenhoax@outlook.com>
2020-09-27 13:57:20 -0500

    Updates

--- src/test/anewlogo.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import Logo from "../components/logo/Logo.jsx";
-
-test("renders learn react link", () => {
-  const { getByText } = render(<Logo />);
-  const logo = getByText(/V/);
-  expect(logo).toBeInTheDocument();
-});
